9eaec80da573f76064cc1e7f69302ac4d32499a2
sato_shie <shiespam@gmail.com>
2022-04-25 20:50:42 -0700

    Update Blurb.js

--- src/components/Blurb.js
+++ src/components/Blurb.js
@@ -14,17 +14,8 @@ class Blurb extends Component
                 <Faq 
                     question="Why register a subdomain via ENSPortal?"
                     answer={[
-                        `Getting a subdomain means you won't have to remember your long hexidecimal blockchain 
-                        address. Instead, you can be identified with a human-readable and memorable name - like  ${' '}
-                        <code>ensportal.eth</code>. If you change your mind about a domain, then you can choose another one 
-                        to be identified by.`,
-                        `It is cheaper - there is only a one time fee, then it's yours for life! If you were to register a ${' '}
-                        top-level domain, you'd have to pay a variable price depending on length and commit to renewing it every year.`,
-                        `You'll never have to remember to renew it! With ENSPortal, we tell you how long the domain will ${' '}
-                        last for - usually for multiple years at one single cost (and anybody can fund a renewal of the domain when the time comes).`,
-                        `With an ENS name, you can <a href="https://medium.com/the-ethereum-name-service/ens-launches-multi-coin-support-15-wallets-to-integrate-92518ab20599" target="_blank">attach different blockchain addresses</a> ${' '}
-                        to it to make your human friendly wallet name work across blockchains (depending on your wallet software). This has great benefits ${' '}
-                        as you can just keep one human-readable name to tell people to send money to!`
+                        `TESTING CHANGES
+                        `
                     ]}
                 />
 
